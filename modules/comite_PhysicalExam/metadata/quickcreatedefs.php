88619bfc113a1b0f0b9f02fc91d42d88ffd283df
Hirak Chattopadhyay <hirak@osscube.com>
2014-05-16 16:42:22 +0530

    SLA-12 : layout change

--- modules/comite_PhysicalExam/metadata/quickcreatedefs.php
+++ modules/comite_PhysicalExam/metadata/quickcreatedefs.php
@@ -30,6 +30,10 @@ array (
         array (
           0 => 'name',
         ),
+        1 => 
+         array(
+            0 => 'description',
+        )
       ),
       'lbl_quickcreate_panel1' => 
       array (
